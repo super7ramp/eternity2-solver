046fc4ba8d615e9edc3226304f54904e9dd68932
Antoine Belvire <antoine.belvire@opensuse.org>
2024-08-16 22:24:11 +0200

    test: Add original problem

--- src/test/java/re/belv/eternity2/solver/SolverTest.java
+++ src/test/java/re/belv/eternity2/solver/SolverTest.java
@@ -1,10 +1,19 @@
 package re.belv.eternity2.solver;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import static java.util.function.Predicate.not;
 import static org.assertj.core.api.Assertions.assertThat;
 import static re.belv.eternity2.solver.Piece.Rotation.*;
 
@@ -74,18 +83,17 @@ final class SolverTest {
 
     @Test
     void solve_5x5() {
-        final var piece19 = new Piece(19, 4, 8, 6, 8);
         final var pieces = new Piece[]{
                 new Piece(0, 1, 0, 2, 1), new Piece(1, 1, 2, 4, 1), new Piece(2, 1, 2, 0, 1), new Piece(3, 1, 2, 0, 1), new Piece(4, 1, 3, 5, 4),
                 new Piece(5, 1, 0, 8, 2), new Piece(6, 1, 3, 8, 2), new Piece(7, 1, 4, 5, 0), new Piece(8, 1, 3, 6, 0), new Piece(9, 1, 2, 5, 2),
                 new Piece(10, 1, 3, 6, 2), new Piece(11, 1, 0, 7, 3), new Piece(12, 1, 4, 6, 3), new Piece(13, 1, 2, 7, 2), new Piece(14, 1, 0, 6, 3),
-                new Piece(15, 1, 2, 5, 3), new Piece(16, 6, 8, 4, 5), new Piece(17, 4, 8, 5, 5), new Piece(18, 6, 8, 7, 6), piece19,
+                new Piece(15, 1, 2, 5, 3), new Piece(16, 6, 8, 4, 5), new Piece(17, 4, 8, 5, 5), new Piece(18, 6, 8, 7, 6), new Piece(19, 4, 8, 6, 8),
                 new Piece(20, 6, 7, 6, 7), new Piece(21, 6, 5, 8, 4), new Piece(22, 5, 8, 5, 8), new Piece(23, 5, 7, 7, 7), new Piece(24, 6, 6, 6, 5),
         };
         final var initialBoard = new Piece[][]{
                 {null, null, null, null, null},
                 {null, null, null, null, null},
-                {null, null, piece19.rotate(PLUS_90), null, null},
+                {null, null, pieces[19].rotate(PLUS_90), null, null},
                 {null, null, null, null, null},
                 {null, null, null, null, null},
         };
@@ -101,5 +109,44 @@ final class SolverTest {
                         {pieces[3].rotate(PLUS_270), pieces[14].rotate(PLUS_180), pieces[4].rotate(PLUS_180), pieces[7].rotate(PLUS_180), pieces[0].rotate(PLUS_180)},
                 })
                 .hasSize(16);
+    }
+
+    @Test
+    @Disabled("too hard")
+    void solve_16x16() throws IOException, URISyntaxException {
+        final Piece[] pieces = readFps("EternityII-256-Bis-ORIGINAL.fps");
+        final Piece[][] initialBoard = IntStream.range(0, 16)
+                .mapToObj(i -> new Piece[16])
+                .toArray(Piece[][]::new);
+
+        final Iterator<Piece[][]> solutionIterator = solver.solve(pieces, initialBoard);
+
+        if (solutionIterator.hasNext()) {
+            System.out.println(Arrays.deepToString(solutionIterator.next()));
+        } else {
+            System.out.println("No solution!");
+        }
+    }
+
+    private static Piece[] readFps(final String fileName) throws URISyntaxException, IOException {
+        final Path filePath = Path.of(SolverTest.class.getResource("/" + fileName).toURI());
+        try (final Stream<String> lines = Files.lines(filePath)) {
+            return lines
+                    .dropWhile(not("[Bordure]"::equals))
+                    .skip(1)
+                    .filter(line -> line.contains("="))
+                    .map(line -> {
+                        final int equalsIndex = line.indexOf('=');
+                        final int id = Integer.parseInt(line.substring(0, equalsIndex)) - 1;
+                        final String[] borderColors = line.substring(equalsIndex + 1).split("-");
+                        final int north = Integer.parseInt(borderColors[0]) - 1;
+                        final int east = Integer.parseInt(borderColors[1]) - 1;
+                        final int south = Integer.parseInt(borderColors[2]) - 1;
+                        final int west = Integer.parseInt(borderColors[3]) - 1;
+                        return new Piece(id, north, east, south, west);
+                    })
+                    .toArray(Piece[]::new);
+        }
+
     }
 }
