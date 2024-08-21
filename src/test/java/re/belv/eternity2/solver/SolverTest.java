c093840f53246f46ef9ead6bccadf7f741958ade
Antoine Belvire <antoine.belvire@opensuse.org>
2024-08-21 08:25:05 +0200

    test: Remove unused import

--- src/test/java/re/belv/eternity2/solver/SolverTest.java
+++ src/test/java/re/belv/eternity2/solver/SolverTest.java
@@ -10,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.function.Predicate.not;
