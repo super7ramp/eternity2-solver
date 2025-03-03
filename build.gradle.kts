c3231e88cab77922722b5bb9e7d14139dee6f546
Antoine Belvire <antoine.belvire@opensuse.org>
2025-03-03 19:58:42 +0100

    chore: Fix JUnit dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.12.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.27.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 java {
